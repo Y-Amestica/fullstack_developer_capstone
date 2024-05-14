89ca8ed5f4032a7a0963b1c2de175d11fd710d52
Y-Amestica <y.amesticaaranguiz@uandresbello.edu>
2024-05-14 17:23:46 -0400

    update

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,7 +1,9 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register/Register";
+import Dealer from "./components/Dealers/Dealer";
 import Dealers from './components/Dealers/Dealers';
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
     </Routes>
   );
 }
