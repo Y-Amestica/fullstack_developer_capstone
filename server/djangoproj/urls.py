7354165dfad53117fe135fdb202ad88558056cf9
Yazmin Arlette Amestica Aranguiz <146291409+Y-Amestica@users.noreply.github.com>
2024-06-28 23:53:13 -0400

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -95,10 +95,14 @@ DATABASES = {
 }
 
 # AUTH_PASSWORD_VALIDATORS = [
-#    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-#    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-#    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-#    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+# {'NAME': 'django.contrib.auth.password_
+# validation.UserAttributeSimilarityValidator'},
+# {'NAME': 'django.contrib.auth.password_
+# validation.MinimumLengthValidator'},
+# {'NAME': 'django.contrib.auth.password_
+# validation.CommonPasswordValidator'},
+# {'NAME': 'django.contrib.auth.password_
+# validation.NumericPasswordValidator'},
 # ]
 
 LANGUAGE_CODE = 'en-us'
