ef9128fadc5ad587b7180825e990fbfe5c28698d
Y-Amestica <y.amesticaaranguiz@uandresbello.edu>
2024-06-28 21:50:44 -0400

    pylint

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,5 +1,6 @@
 from .models import CarMake, CarModel
 
+
 def initiate():
     car_make_data = [
         {"name": "NISSAN", "description": "Great cars. Japanese technology"},
@@ -13,7 +14,7 @@ def initiate():
     for data in car_make_data:
         car_make_instances.append(
             CarMake.objects.create(
-                name=data['name'], 
+                name=data['name'],
                 description=data['description']
             )
         )
@@ -116,6 +117,6 @@ def initiate():
         CarModel.objects.create(
             name=data['name'],
             car_make=data['car_make'],
-            type=data['type'], 
+            type=data['type'],
             year=data['year']
         )
