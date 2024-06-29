2e0ebca8d6771f31124bbe41bd9ad70cb3e601c5
Yazmin Arlette Amestica Aranguiz <146291409+Y-Amestica@users.noreply.github.com>
2024-06-28 21:58:22 -0400

    pylint models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,18 +2,16 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
- 
+
     def __str__(self):
         return self.name  # Return the name as the string representation
 
-
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
